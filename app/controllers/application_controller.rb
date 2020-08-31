aa605cc21888b35234d40fe1a6d90fb5038b5c78
takaturnsaru <69668280+takaturnsaru@users.noreply.github.com>
2020-08-31 14:32:45 +0900

    セーブ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :comfigure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_samitizer.permit(:sign_up, key: [:name])
+  end
+
 end
